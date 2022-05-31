b510e0d086aa3bd1f8fa03faa50e1a9fcd3caace
cenquanyu <cenquanyu@baidu.com>
2022-05-31 22:49:12 +0800

    20220531，二分查找

---
+++ 二分查找/q704-Search-二分查找-mid.go
@@ -0,0 +1,17 @@
+package 二分查找
+
+func search(nums []int, target int) int {
+	low := nums[0]
+	high := len(nums) - 1
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
